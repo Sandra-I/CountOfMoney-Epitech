d3b1a0de216cf38c62265f3a77047d6213f3c6d4
Sandra-I <sandra.ibrahim@hotmail.fr>
2020-11-22 12:20:43 +0100

    feat(admin) : admin page creation + modal add crypto

---
+++ frontend/components/AddCryptoCurrency.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-dialog
+    modal-append-to-body
+    title="Tips"
+    :visible.sync="dialogVisible"
+    width="30%"
+    :before-close="handleClose"
+  >
+    <span>Ceci est un message</span>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible = false">Annuler</el-button>
+      <el-button type="primary" @click="dialogVisible = false"
+        >Confirmer</el-button
+      >
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name: AddCryptoCurrency,
+  data() {
+    return {
+      dialogVisible: false
+    };
+  },
+  methods: {
+    handleClose(done) {
+      this.$confirm("Voulez-vous vraiment quitter ?")
+        .then(_ => {
+          done();
+        })
+        .catch(_ => {});
+    }
+  }
+};
+</script>
---
+++ frontend/pages/admin.vue
@@ -0,0 +1,67 @@
+<template>
+  <el-container>
+    <el-header height="120px">
+      <h1>Admin Settings</h1>
+      <p>All the modifications will affect the entire app!</p>
+    </el-header>
+    <el-main>
+      <el-header>
+        <el-row class="d-flex">
+          <el-col :span="12" class="d-flex">
+            <h2>Actual Cryptocurrencies</h2>
+          </el-col>
+          <el-col :span="12" class="d-flex justify-content-end">
+            <el-button type="default" icon="el-icon-plus" @click="showModale = true">ADD CRYPTO</el-button>
+          </el-col>
+        </el-row>
+      </el-header>
+      <!-- ajouter confirmation avant la suppression -->
+      <Crypto-table />
+    </el-main>
+
+    <!-- Modal test here // append-to-body ?!-->
+    <el-dialog
+      append-to-body
+      title="Add a new cryptocurrencie"
+      :visible.sync="showModale"
+      width="70%"
+    >
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="closeModale">CANCEL</el-button>
+        <!-- @click="" méthode pour valider ajout crypto -->
+        <el-button type="primary" 
+          >ADD</el-button
+        >
+      </span>
+    </el-dialog>
+  </el-container>
+</template>
+
+<script>
+import CryptoTable from '../components/CryptoTable.vue';
+
+export default {
+  components: { CryptoTable },
+  data() {
+    return {
+      showModale: false
+    };
+  },
+  methods: {
+    openModale() {
+      return this.showModale = true;
+    },
+    closeModale() {
+      return this.showModale = false;
+    },
+    // voir si nécessaire
+    // handleClose(done) {
+    //   this.$confirm("Do you really want to add this currency?")
+    //     .then(_ => {
+    //       done();
+    //     })
+    //     .catch(_ => {});
+    // }
+  }
+}
+</script>
