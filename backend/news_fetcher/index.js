ec8679a8c73249187e8a7050e5006b7fcd17aa0a
Patrice Bistouquette <thomas.boursier@epitech.eu>
2020-12-10 21:04:09 +0100

    working news fetcher but code is ugly

--- backend/news_fetcher/index.js
+++ backend/news_fetcher/index.js
@@ -15,16 +15,13 @@ const connection = mysql.createConnection({
 
 connection.connect(error => {
     if (error) throw error
-    else console.log("Successfully connected to the database.");
+    else console.log("Successfully connected to the database."); //TODO: virer
 });
 
+//TODO: while?
 
-connection.query('DELETE FROM articles WHERE date < DATE_SUB(NOW() , INTERVAL 7 DAY)', function (err, res) {
-    if (err) {
-        console.error(err)
-    } else if (res.length > 0) {
-        console.log("Deleted " + res.length + " outdated entries.")
-    }
+connection.query('DELETE FROM articles WHERE date < DATE_SUB(NOW() , INTERVAL 7 DAY)', function (err) {
+    if (err) throw err
 })
 
 axios.get('https://min-api.cryptocompare.com/data/v2/news/', {
@@ -32,63 +29,65 @@ axios.get('https://min-api.cryptocompare.com/data/v2/news/', {
         'Authorization': `Apikey: ${apiKey}`
     }
 }).then((res) => {
-    let lastDate = 0
-    let lastDateElements = []
+    let lastDate = {
+        time: 0,
+        elements: []
+    }
     let existing = false
     let current = {}
 
     connection.query('SELECT * FROM articles ORDER BY date DESC', function (err, sqlRes) {
-        if (err) {
-            console.error(err)
-        } else if (sqlRes.length > 0) {
-            lastDate = moment(sqlRes[0].date, "YYYY-MM-DD HH:MM:SS").unix()
-            lastDateElements.push(sqlRes[0])
-            for (let i = 1; i < sqlRes.length; i++) {
-                if (moment(sqlRes[i].date, "YYYY-MM-DD HH:MM:SS").unix() === lastDate) {
-                    lastDateElements.push(sqlRes[i])
-                }
-            }
-        }
-    })
+        if (err) throw err
+        else if (sqlRes.length > 0) {
 
-    for (let i = 1; i < res.data.length; i++) {
-        current = {
-            title: res.data[i].title,
-            summary: res.data[i].categories,
-            source: res.data[i].source,
-            date: moment(res.data[i].published_on).format("YYYY-MM-DD HH:MM:SS"),
-            urlpage: res.data[i].url,
-            urlimage: res.data[i].imageurl
-        }
-        if (res.data[i].published_on > lastDate) {
-            connection.query('INSERT INTO articles SET ?', current, function(err) {
-                if (err) {
-                    console.error(err)
-                }
-            })
-        } else if (res.data[i].published_on === lastDate) {
-            for (let j = 0; j < lastDateElements.length; j++) {
-                if (res.data[i].url === lastDateElements[j].urlpage) {
-                    existing = true
+            lastDate.time = moment(sqlRes[0].date, "YYYY-MM-DD HH:mm:ss").unix()
+            lastDate.elements.push(sqlRes[0])
+            for (let i = 1; i < sqlRes.length; i++) {
+                if (moment(sqlRes[i].date, "YYYY-MM-DD HH:mm:ss").unix() === lastDate.time) {
+                    lastDate.elements.push(sqlRes[i])
                 }
             }
-            if (existing === false) {
+
+        }
+
+        for (let i = res.data.Data.length - 1; i > 0; i--) {
+
+            current = {
+                title: res.data.Data[i].title,
+                summary: res.data.Data[i].categories,
+                source: res.data.Data[i].source,
+                date: moment(res.data.Data[i].published_on * 1000).format("YYYY-MM-DD HH:mm:ss"),
+                urlpage: res.data.Data[i].url,
+                urlimage: res.data.Data[i].imageurl
+            }
+
+            if (res.data.Data[i].published_on > lastDate.time) {
+
                 connection.query('INSERT INTO articles SET ?', current, function(err) {
-                    if (err) {
-                        console.error(err)
-                    }
+                    if (err) throw err
                 })
+
+            } else if (res.data.Data[i].published_on === lastDate.time) {
+
+                for (let j = 0; j < lastDate.elements.length; j++) {
+                    if (res.data.Data[i].url === lastDate.elements[j].urlpage) {
+                        existing = true
+                    }
+                }
+                if (existing === false) {
+                    connection.query('INSERT INTO articles SET ?', current, function(err) {
+                        if (err) throw err
+                    })
+                }
+                existing = false
+
             }
-            existing = false
         }
-    }
-    connection.end()
-    console.log('Restarting in 30 seconds.')
+        connection.end()
+    })
 }).catch((error) => {
     console.error(error)
 })
 
 
-setTimeout(function () {
-    console.log('Restarting...');
-}, 30000)
+setTimeout(function () {}, 10000)
